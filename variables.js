6700a9cddff1d3607ab5f32c31fb99c71505071f
Andrés Pérez <andresfe52@gmail.com>
2020-10-22 07:35:36 -0500

    variables

---
+++ variables.js
@@ -0,0 +1,10 @@
+//en memoria se guardan los valores que creamos a travez de las variables
+
+//declarar e inicializar la variable
+var nombre = "Andrés";
+
+//Declarar una variable
+var edad;
+
+//Inicializar variables
+edad = 32;
