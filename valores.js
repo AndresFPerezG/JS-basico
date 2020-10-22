10f19594cd9912d49c5fe52fd893e53d46fcd388
Andrés Pérez <andresfe52@gmail.com>
2020-10-22 07:28:58 -0500

    valores en javascript

---
+++ valores.js
@@ -0,0 +1,19 @@
+//valores int
+40
+
+//valores de string
+"Andrés Pérez"
+
+//valores booleanos
+true
+false
+
+//valores vacios o placeholder
+null
+undefined //valor reservado de javascript
+
+//valor de tió array (valores primitivos)
+[1,2,3]
+
+//valor de tipó objeto
+{nombre: "Andrés"}
