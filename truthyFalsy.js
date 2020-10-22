fe1884d99b720ad38307022914dc832f25b210ea
Andrés Pérez <andresfe52@gmail.com>
2020-10-22 10:14:47 -0500

    adición a true

--- truthyFalsy.js
+++ truthyFalsy.js
@@ -7,6 +7,7 @@ Boolean(false); //false
 Boolean(""); //false
 
 //Ejemplos en los que Boolean devuelve verdadero:
+Boolean(-1); //true
 Boolean(1); //true para 1 o cualquier número diferente de cero (0)
 Boolean("a"); //true para cualquier caracter o espacio en blanco en el string
 Boolean([]); //true aunque el array esté vacío
